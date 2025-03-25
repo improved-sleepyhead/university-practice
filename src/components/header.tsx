5091391641e9544e8da2a90974ec91d5cf3e10a0
Ivan <ivan18drozdov@gmail.com>
2025-03-25 17:44:15 +0300

    started to rewrite market app to the web-gallery

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 
 export const Header = () => {
   return (
-    <header className="p-4 bg-blue-400 flex justify-center gap-x-10">
+    <header className="p-4 flex justify-center gap-x-10">
       <h1 className="text-xl font-bold text-white">Web Market</h1>
     </header>
   );
